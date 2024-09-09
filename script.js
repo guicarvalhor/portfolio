bb015d3531914556317ba819781ed27edd32dad4
Guilherme Rodrigues <carvalho.guilhermer@gmail.com>
2024-09-08 21:00:49 -0300

    Criação da home page

---
+++ script.js
@@ -0,0 +1,42 @@
+function goToProject() {
+    // Redireciona o usuário para a página do projeto
+    window.location.href = "emconstrucao.html";
+}
+function toggleMenu() {
+    const navMenu = document.querySelector('.nav-menu');
+    navMenu.classList.toggle('active');
+  } const cargoElement = document.getElementById('cargo');
+  const cargos = ['Desenvolvedor', 'Design UX/UI', 'Product Designer', 'Apaixonado por tecnologia'];
+  let currentCargoIndex = 0;
+  let currentCharIndex = 0;
+  let typing = true;
+
+  function type() {
+      if (typing) {
+          if (currentCharIndex < cargos[currentCargoIndex].length) {
+              cargoElement.textContent += cargos[currentCargoIndex][currentCharIndex];
+              currentCharIndex++;
+              setTimeout(type, 100); // Velocidade de digitação
+          } else {
+              typing = false;
+              setTimeout(() => {
+                  cargoElement.textContent = '';
+                  currentCharIndex = 0;
+                  typing = true;
+                  currentCargoIndex = (currentCargoIndex + 1) % cargos.length;
+                  type(); // Começa a digitar o próximo cargo
+              }, 1000); // Tempo de pausa antes de apagar o texto
+          }
+      } else {
+          if (cargoElement.textContent.length > 0) {
+              cargoElement.textContent = cargoElement.textContent.slice(0, -1);
+              setTimeout(type, 50); // Velocidade de apagamento
+          } else {
+              typing = true;
+              currentCargoIndex = (currentCargoIndex + 1) % cargos.length;
+              setTimeout(type, 100); // Começa a digitar o próximo cargo
+          }
+      }
+  }
+
+  type(); // Inicia a função de digitação
